fb2c1f1b51d43f31d7140c9fcf3728789663b4ff
Esha Rana <ranaesha520@gmail.com>
2026-01-09 15:44:13 +0545

    GUI update

--- Queue/GUI.py
+++ Queue/GUI.py
@@ -1,24 +1,22 @@
 import tkinter as tk
 from Logic import park_car, remove_car, get_parking_status
 
-# ---------- ROOT WINDOW ----------
 root = tk.Tk()
 root.title("Parking Queue Simulator")
 root.geometry("520x400")
 
-# ---------- CANVAS ----------
 canvas = tk.Canvas(root, width=500, height=200, bg="lightgray")
 canvas.pack(pady=10)
 
-# ---------- INPUT ----------
-entry = tk.Entry(root, font=("Arial", 12))
-entry.pack(pady=5)
+label = tk.Label(root, text="Enter Car Number:")
+label.pack(side=tk.LEFT, padx=5, pady=5)
 
-# ---------- STATUS LABEL ----------
+entry = tk.Entry(root, font=("Arial",12))
+entry.pack(side=tk.LEFT, padx=5, pady=5)
+
 status_label = tk.Label(root, text="", font=("Arial", 11))
 status_label.pack(pady=5)
 
-# ---------- DRAW PARKING ----------
 def draw_parking():
     canvas.delete("all")
     parking = get_parking_status()
@@ -30,6 +28,7 @@ def draw_parking():
         y2 = 120
 
         canvas.create_rectangle(x1, y1, x2, y2, fill="white")
+        canvas.create_text((x1 + x2)//2, y1 - 10, text=f"Slot {i}", font=("Arial", 10, "bold"))
 
         if parking[i] is not None:
             canvas.create_text(
@@ -37,9 +36,9 @@ def draw_parking():
                 (y1 + y2) // 2,
                 text=parking[i],
                 font=("Arial", 10)
+
             )
 
-# ---------- BUTTON ACTIONS ----------
 def park():
     car = entry.get()
     if car:
@@ -53,15 +52,13 @@ def remove():
     status_label.config(text=msg)
     draw_parking()
 
-# ---------- BUTTONS ----------
+
 btn_frame = tk.Frame(root)
 btn_frame.pack(pady=10)
 
 tk.Button(btn_frame, text="Park Car", width=12, command=park).grid(row=0, column=0, padx=5)
 tk.Button(btn_frame, text="Remove Car", width=12, command=remove).grid(row=0, column=1, padx=5)
 
-# ---------- INITIAL DRAW ----------
 draw_parking()
 
-# ---------- MAIN LOOP ----------
 root.mainloop()
